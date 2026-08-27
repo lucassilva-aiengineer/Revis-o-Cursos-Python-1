d4645e94a5ef42319140474c1301a02d2109fe2c
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-08-27 13:15:12 -0300

    Continuando o curso 1

---
+++ curso_python_1/basics.py
@@ -0,0 +1,12 @@
+# Variáveis 
+
+name = "Beau" ; print("Meu nome é Marcos!")
+
+#   print("Indentação")
+
+# Indica o escopo de um bloco de código.
+
+# Tipos de dados 
+
+name = "Mateus" 
+print(type(name))
