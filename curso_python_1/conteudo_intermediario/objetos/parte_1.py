fca79a1c0b7d6d563e64b4a75fff10c47ef80249
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-09-21 14:15:37 -0300

    Adicionando conteúdos ao curso

---
+++ curso_python_1/conteudo_intermediario/objetos/parte_1.py
@@ -0,0 +1,29 @@
+# Objetos 
+
+def objetos_numéricos():
+
+    # Um objeto numérico 
+    idade = 8 
+
+    # listas 
+    print(idade.real)
+    print(idade.imag)
+    print(idade.bit_length())
+
+
+def objeto_lista():
+
+
+    itens = [1, 2]
+    itens.append(3)
+    itens.append(4)
+
+    print(id(itens)) # Endereço de memória do objeto 
+
+
+def main():
+
+    ... 
+
+if __name__ == '__main__':
+    main()
